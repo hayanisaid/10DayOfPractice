030e35bfda80bdffa7b585da7ec9a00b9fd6b50b
hayani said <hayanisaid1995@gmail.com>
2018-03-28 21:58:09 +0100

    add js function to video playlist

--- video-playlist/js/app.js
+++ video-playlist/js/app.js
@@ -1,12 +1,40 @@
 
-var videos=document.getElementsByClassName('item');
+// var video=document.getElementById('playvid');
+//  video.addEventListener('click',function(){
+//  	videCnt=document.getElementById('srcVideo')
+//  	videCnt.play();
+//  	console.log(videCnt.duration % 100)
+//  })
+window.onload=(e)=>{
+ setDefaultDuration();
+}
+ 
+var videos=document.getElementsByClassName('video-item');
+var VideoList=[
+ './videos/video1.mp4',
+  './videos/video2.mp4',
+   './videos/video3.mp4',
+    './videos/video4.mp4'];
+    videos.onclick=function(e){
+    	alert(e)
+    }
 
- for (let i=0 ;i <= videos.length; i++){
+   for (let i=0 ;i <= videos.length; i++){
+ 	 
  	videos[i].addEventListener('click',(e)=>{
- 		//console.log(activeVideo.src + '==>>current Src')
- 		let targetSrc=e.target.src + 'this is target src';
- 	   let activeVideo=document.getElementById('srcVideo').src=e.target.src;
- 	  //console.log(activeVideo.src + '==>new srcc')
- 	  //activeVideo.play();
+
+  	   let activeVideo=document.getElementById('srcVideo').src= VideoList[i];
+  	   videCnt=document.getElementById('srcVideo')
+		videCnt.play();
+ 	    
  	})
- }
+  }
+
+
+function setDefaultDuration(){
+	var videos=document.getElementsByClassName('video-item');
+	for(let i=0; i <= videos.length ;i++){
+		videos[i].currentTime=2;
+	}
+}
+
